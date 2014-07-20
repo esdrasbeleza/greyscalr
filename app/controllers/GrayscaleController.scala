102a741ef34bc2c2e3b665419b72c48bd25503d6
Esdras Beleza <github@contato.esdrasbeleza.com>
2014-07-20 12:28:06 -0300

    Move image conversion from model to controller

--- app/controllers/GrayscaleController.scala
+++ app/controllers/GrayscaleController.scala
@@ -5,6 +5,7 @@ import java.io.File
 import java.util.UUID
 import models.ImageOperationStatus
 import play.api.libs.json.Json._
+import javax.imageio.ImageIO
 
 object GrayscaleController extends Controller {
 
@@ -20,9 +21,34 @@ object GrayscaleController extends Controller {
 
         files.head.ref.moveTo(new File(fullPath))
         val status = ImageOperationStatus.create(uuid, fullPath)
+        convertImageToGrayScale(fullPath, "/tmp/" + uuid + ".png")
         Created(toJson(status))
       }
     }
+  }
+
+  def convertImageToGrayScale(input: String, output: String) = {
+    val image = ImageIO.read(new File(input))
+
+    val width = image.getWidth
+    val height = image.getHeight
+
+    for (
+      i <- 0 to (width - 1);
+      j <- 0 to (height - 1)
+    ) {
+      val pixel = image.getRGB(i, j)
+
+      val red = (pixel >> 16) & 255
+      val green = (pixel >> 8) & 255
+      val blue = pixel & 255
+
+      val newBlue, newGreen, newRed = (blue + green + red) / 3
+      val newRgbValue = (newRed << 16) + (newGreen << 8) + (newBlue)
+
+      image.setRGB(i, j, newRgbValue)
+    }
+    ImageIO.write(image, "PNG", new File(output))
   }
 
 }
--- app/models/ImageOperationStatus.scala
+++ app/models/ImageOperationStatus.scala
@@ -4,8 +4,6 @@ import com.mongodb.casbah.Imports._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import db.MongoAccess.{ insert => insertInMongo }
-import javax.imageio.ImageIO
-import java.io.File
 import play.api.Logger
 
 object ImageOperationStatus {
@@ -26,38 +24,11 @@ object ImageOperationStatus {
     )(unlift(unapply))
 
   def create(id: String, localPath: String) = {
-    val imageOperation = MongoDBObject("status" -> StatusConverting,
+    val imageOperation = MongoDBObject("_id" -> id,
+                                       "status" -> StatusConverting,
                                        "localPath" -> localPath)
-
     insertInMongo(imageOperation)
-    convertImageToGrayScale(localPath, "/tmp/" + id + ".png")
-
     new ImageOperationStatus(id, StatusConverting, None)
-  }
-
-  def convertImageToGrayScale(input: String, output: String) = {
-    logger.debug(s"Reading $input")
-    val image = ImageIO.read(new File(input))
-
-    val width = image.getWidth
-    val height = image.getHeight
-
-    for (
-      i <- 0 to (width - 1);
-      j <- 0 to (height - 1)
-    ) {
-      val pixel = image.getRGB(i, j)
-
-      val red = (pixel >> 16) & 255
-      val green = (pixel >> 8) & 255
-      val blue = pixel & 255
-
-      val newBlue, newGreen, newRed = (blue + green + red) / 3
-      val newRgbValue = (newRed << 16) + (newGreen << 8) + (newBlue)
-
-      image.setRGB(i, j, newRgbValue)
-    }
-    ImageIO.write(image, "PNG", new File(output))
   }
 
 }
